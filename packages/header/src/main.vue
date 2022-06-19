ef119d067d20c3244f3aa8183a2e306acc95ef5d
hanxian韩闲 <woow.wu7@gmail.com>
2022-06-19 23:02:45 +0800

    feat: header.

---
+++ packages/header/src/main.vue
@@ -0,0 +1,17 @@
+<template lang="">
+  <header class="header" :style="{ height: height + 'px' }"></header>
+</template>
+<script>
+export default {
+  name: "ElHeader",
+  componentName: "ElHeader",
+
+  props: {
+    height: {
+      type: [Number, String],
+      default: 60,
+    },
+  },
+};
+</script>
+
